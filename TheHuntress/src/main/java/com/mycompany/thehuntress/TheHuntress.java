53c9eaba48c941cf111f2b2bd8a96b11640115a0
Lenny <100688028+Isasanelli@users.noreply.github.com>
2022-05-06 23:00:05 +0200

    Code
    Code of game

---
+++ TheHuntress/src/main/java/com/mycompany/thehuntress/TheHuntress.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.thehuntress;
+
+/**
+ *
+ * @author The Huntress
+ * @version 06/05/2022
+ */
+public class TheHuntress {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
