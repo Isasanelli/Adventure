e917f80b4e6a6578c034829aa2bcd77f49cf1dff
Lenny <100688028+Isasanelli@users.noreply.github.com>
2022-05-06 22:58:26 +0200

    Add files via upload

--- TheHuntress/src/main/java/com/mycompany/thehuntress/TheHuntress.java
+++ TheHuntress/src/main/java/com/mycompany/thehuntress/TheHuntress.java
@@ -3,11 +3,12 @@ package com.mycompany.thehuntress;
 
 /**
  *
- * @author sasan
+ * @author The Huntress
+ * @version 06/05/2022
  */
 public class TheHuntress {
 
     public static void main(String[] args) {
-        System.out.println("THE HUNTRESS");
+        System.out.println("Hello World");
     }
 }
