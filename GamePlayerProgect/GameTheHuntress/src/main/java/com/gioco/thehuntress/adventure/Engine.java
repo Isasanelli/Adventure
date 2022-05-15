8dd3832afded4f4fffe5abb51ac5612a29a6eaa1
Lenny <100688028+Isasanelli@users.noreply.github.com>
2022-05-15 17:40:19 +0200

    UpCode

---
+++ GamePlayerProgect/GameTheHuntress/src/main/java/com/gioco/thehuntress/adventure/Engine.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.gioco.thehuntress.adventure;
+
+/**
+ *
+ *  @author Margari Chiara
+ * @author Ricciardi Raffaella
+ * @author Sasanelli Ilenia
+ */
+public class Engine {
+
+    public static void main(String[] args) {
+        Grafica menu = new Grafica();
+        menu.writeMenu();
+    }
+}
