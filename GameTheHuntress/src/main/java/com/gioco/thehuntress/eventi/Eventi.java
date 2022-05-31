f56378959a684d633504fd27d420f13803b275ca
Chiara Margari <margarichiara2001@gmail.com>
2022-05-31 20:17:48 +0200

    Sistemazione dei metodi leggiComando e leggiRegole

--- GameTheHuntress/src/main/java/com/gioco/thehuntress/eventi/Eventi.java
+++ GameTheHuntress/src/main/java/com/gioco/thehuntress/eventi/Eventi.java
@@ -3,36 +3,20 @@ package com.gioco.thehuntress.eventi;
 import java.io.*;
 
 public class Eventi {
-    public static void leggiRegole()throws FileNotFoundException, IOException{
+    public static final String patFileRegole="file//regole.txt";
+    public static final String patFileComandi="file//comandi.txt";
 
-    try{
-        File fileDaLeggere= new File("file//regole.txt");
-        BufferedReader regole= new BufferedReader(new FileReader(fileDaLeggere ));
-        String lineaSingola;
-
-        do{
-            lineaSingola=regole.readLine();
-            if (lineaSingola!=null){
-                System.out.println (lineaSingola);
-            }
-        }while ( lineaSingola != null);
-
-    }catch (
-    FileNotFoundException e ){
-        System.err.println("il file non esiste");
-
-    }catch (
-    IOException e ){
-        System.err.println("ERRORE  DI I/0");
-
-
+    public static void leggiRegole() throws FileNotFoundException, IOException{
+        leggiFile(patFileRegole);
     }
-}
 
-    public static void leggiComandi()throws FileNotFoundException, IOException{
+    public static void leggiComandi() throws FileNotFoundException, IOException{
+        leggiFile(patFileComandi);
+    }
 
+    public static void leggiFile(String stringaPat) throws FileNotFoundException, IOException{
         try{
-            File fileDaLeggere= new File ("file//comandi.txt");
+            File fileDaLeggere= new File (stringaPat);
             BufferedReader regole= new BufferedReader(new FileReader(fileDaLeggere ));
             String lineaSingola;
 
